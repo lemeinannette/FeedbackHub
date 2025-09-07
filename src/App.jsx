799f143aeaedef486d53324789b376fea88f4256
lemeinannette <emaillemeinannette@gmail.com>
2025-09-07 12:31:45 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,3 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import FeedbackForm from "./components/FeedbackForm";
-import ThankYouScreen from "./components/ThankYouScreen";
-import AdminPanel from "./components/AdminPanel";
-import AdminLogin from "./components/AdminLogin";
-
 export default function App() {
-  const isAdminLoggedIn = localStorage.getItem("isAdminLoggedIn") === "true";
-
-  return (
-    <Router>
-      <Routes>
-        {/* Guest feedback routes */}
-        <Route path="/" element={<FeedbackForm />} />
-        <Route path="/thank-you" element={<ThankYouScreen />} />
-
-        {/* Admin routes */}
-        <Route path="/admin-login" element={<AdminLogin />} />
-        <Route
-          path="/admin"
-          element={isAdminLoggedIn ? <AdminPanel /> : <Navigate to="/admin-login" />}
-        />
-
-        {/* Fallback (if no route matches) */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
-  );
+  return <h1 style={{ textAlign: "center", marginTop: "50px" }}>Hello, FeedbackHub is working!</h1>;
 }
