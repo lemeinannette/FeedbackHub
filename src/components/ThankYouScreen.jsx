3499ee6e90f8c468106f583e611430bc0d73769f
lemeinannette <emaillemeinannette@gmail.com>
2025-10-03 19:49:40 +0300

    updateed

--- src/components/ThankYouScreen.jsx
+++ src/components/ThankYouScreen.jsx
@@ -9,11 +9,9 @@ export default function ThankYouScreen() {
         <div className="success-icon">
           <i className="bx bx-check-circle"></i>
         </div>
-        <img 
-          src="https://img.icons8.com/color/96/success--v1.png" 
-          alt="Success" 
-          className="success-illustration" 
-        />
+        
+        
+        
         <h1>Thank You for Your Feedback!</h1>
         <p>Your response has been recorded successfully.</p>
         <Link to="/" className="back-button">
